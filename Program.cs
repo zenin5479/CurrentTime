5baf093e78731f14e3204978ab2699cff2c11b02
zenin5479 <zenin5479@gmail.com>
2026-01-26 08:53:26 +0300

    Update 1.0 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -17,13 +17,11 @@ namespace CurrentTime
       // Точное время в Unix‑timestamp в миллисекундах (13‑значное число)
       static void CaseOne()
       {
-
-
          // Способ 1
          Console.WriteLine("==========================================");
+         Console.WriteLine("Способ 1 (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()):");
          Console.WriteLine("Текущее UTC время: {0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.UtcNow);
          long timestampone = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-         Console.WriteLine("Способ 1 (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()):");
          Console.WriteLine("Результат: {0}", timestampone);
          Console.WriteLine("Длина: {0} цифр", timestampone.ToString().Length);
          Console.WriteLine("Формат: {0:#,##0}\n", timestampone);
