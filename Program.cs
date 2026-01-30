07814a172613c11ed399e976eb65cd87ad7efd4a
zenin5479 <zenin5479@gmail.com>
2026-01-30 09:15:31 +0300

    Update 12.0 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -29,13 +29,17 @@ namespace CurrentTime
          //В API Binance Все поля, относящиеся ко времени и меткам времени, отображаются в миллисекундах». (в стиле Unix)
          // преобразовать метку времени Unix в DateTime.
 
+         // Подпись : Обеспечивает целостность и подлинность запроса, проверяя, что он был сгенерирован законным пользователем.
+
+         // В репозитории Binance на GitHub binance-signature - examples содержатся примеры, которые помогут пользователям научиться создавать подписи.
+         //long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+         //queryStringBuilder.Append("timestamp=").Append(now);
+
          // timestamp: Представляет собой метку времени в миллисекундах, когда был инициирован запрос
+         // Она может быть включена в строку запроса или тело запроса.
          StringBuilder queryStringBuilder = new StringBuilder();
          long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
          queryStringBuilder.Append("timestamp=").Append(now);
-        
-         //long now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-         //queryStringBuilder.Append("timestamp=").Append(now);
 
          // Ответ сервера Binance
          // Формат и данные
