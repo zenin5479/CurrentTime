9d8ac72c7c3d8e519d13fdb13ff42bc68aef2459
zenin5479 <zenin5479@gmail.com>
2026-01-30 14:33:56 +0300

    Update 14.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace CurrentTime
       public static DateTime BinanceTimeStampToDateTime(double binanceTimeStamp)
       {
          // Binance timestamp is milliseconds past epoch
-         var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+         var epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
          return epoch.AddMilliseconds(binanceTimeStamp);
       }
 
