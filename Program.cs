2118aebcc895d6f98c8bd13819514b1b5dc2ad73
zenin5479 <zenin5479@gmail.com>
2026-02-02 09:19:38 +0300

    Update 21.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -84,9 +84,9 @@ namespace CurrentTime
          DateTime dateTimeNow = DateTime.Now;
 
          // Конвертация в Unix timestamp
-         DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-         TimeSpan timeSpan = dateTimeNow.ToUniversalTime() - unixStart;
-         long timeStampOne = (long)(timeSpan.TotalMilliseconds);
+         DateTime unixStartOne = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+         TimeSpan timeSpanOne = dateTimeNow.ToUniversalTime() - unixStartOne;
+         long timeStampOne = (long)(timeSpanOne.TotalMilliseconds);
          Console.WriteLine("Текущее время: {0}", dateTimeNow);
          Console.WriteLine("Локальное время: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTimeNow);
          Console.WriteLine("Unix timestamp: {0}", timeStampOne);
