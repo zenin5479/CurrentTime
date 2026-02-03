209096f19924e014cf4ad891414b586ee6b1c389
zenin5479 <zenin5479@gmail.com>
2026-02-03 14:03:39 +0300

    Update 29.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -50,35 +50,31 @@ namespace CurrentTime
          Console.WriteLine("Текущее UTC время в милисекундах: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTimeNow);
          Console.WriteLine("Timestamp: {0}", timeStampTwo);
 
-
-         Console.WriteLine("Определение точного локального времени в миллисекундах");
-         // Способ 1: DateTimeOffset (рекомендуется)
-         DateTimeOffset datenow = DateTimeOffset.Now;
-         long timestampoffset = datenow.ToUnixTimeMilliseconds();
-         Console.WriteLine("=== Способ 1: DateTimeOffset ===");
-         Console.WriteLine("Локальное время: {0:dd.MM.yyyy HH:mm:ss.fff}", datenow);
-         Console.WriteLine("Unix timestamp: {0}", timestampoffset);
-
-         // Способ 2: Ручной расчет
-         DateTime datelocalnow = DateTime.Now;
-         DateTime universalnow = datelocalnow.ToUniversalTime();
-         DateTime unixepoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-         long timestampepoch = (long)(universalnow - unixepoch).TotalMilliseconds;
-         Console.WriteLine("=== Способ 2: Ручной расчет ===");
-         Console.WriteLine("Локальное время: {0:dd.MM.yyyy HH:mm:ss.fff}", datelocalnow);
-         Console.WriteLine("Unix timestamp: {0}", timestampepoch);
-
-         // Проверка совпадения
-         Console.WriteLine("Результаты совпадают: {0}", timestampoffset == timestampepoch);
-
-
-
-
-
          // 2. Конвертация Timestamp в DateTime
+         // Создаем начальную дату Unix эпохи
+         DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+         // Конвертируем timestamp в локальное время
+         DateTime localTime = epoch.AddMilliseconds(timeStampOne).ToLocalTime();
+         Console.WriteLine("Базовое преобразование: {0}", localTime);
+         Console.WriteLine("Базовое преобразование с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", localTime);
+
+         // 2. С учетом часового пояса
+         // Свойство TimeZoneInfo.Id - "Russian Standard Time"
+         DateTime utcDateTime = epoch.AddMilliseconds(timestampTwo);
+
+         // Получаем нужный часовой пояс
+         TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
+
+         // Конвертируем в локальное время
+         DateTime localZone = TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, timeZone);
+         Console.WriteLine("С учетом часового пояса: {0}", localZone);
+         Console.WriteLine("С учетом часового пояса с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", localZone);
+
+
+
          Console.WriteLine("=== Конвертация Timestamp в DateTime ===");
          Console.WriteLine("=== Способ 1: DateTime ===");
-         DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
          DateTime addMilliseconds = epoch.AddMilliseconds(timestampOne);
          Console.WriteLine("Конвертация из Timestamp в DateTime: {0}", addMilliseconds);
          Console.WriteLine("Конвертация из Timestamp в DateTime с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", addMilliseconds);
@@ -91,24 +87,7 @@ namespace CurrentTime
 
       public static void CaseTwo()
       {
-         Console.WriteLine("=== DateTime, DateTimeOffset и TimeSpan ===");
-         Console.WriteLine("=== Получение текущего Timestamp через DateTime и TimeSpan ===");
-         DateTime dateTimeNow = DateTime.Now;
-         DateTime unixStartOne = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-         TimeSpan timeSpanOne = dateTimeNow.ToUniversalTime() - unixStartOne;
-         long timeStampOne = (long)(timeSpanOne.TotalMilliseconds);
-         Console.WriteLine("Текущее время в DateTime: {0}", dateTimeNow);
-         Console.WriteLine("Текущее время в DateTime с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTimeNow);
-         Console.WriteLine("Timestamp: {0}", timeStampOne);
-
-         Console.WriteLine("=== Получение текущего Timestamp через DateTimeOffset и TimeSpan с учетом часового пояса ===");
-         DateTimeOffset dateTimeOffset = DateTimeOffset.Now;
-         DateTimeOffset unixStart = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
-         TimeSpan timeSpanTwo = dateTimeOffset.UtcDateTime - unixStart.UtcDateTime;
-         long timestampTwo = (long)(timeSpanTwo.TotalMilliseconds);
-         Console.WriteLine("Текущее время в DateTime: {0}", dateTimeOffset);
-         Console.WriteLine("Текущее время в DateTime с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTimeOffset);
-         Console.WriteLine("Timestamp: {0}", timestampTwo);
+        
 
          // 1. Базовый метод
          // Создаем начальную дату Unix эпохи
