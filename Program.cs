8f9d98197497e2c2598c2041c4b97876317a4cf7
zenin5479 <zenin5479@gmail.com>
2026-02-04 09:59:19 +0300

    Update 30.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -61,6 +61,9 @@ namespace CurrentTime
          Console.WriteLine("Конвертация из Timestamp в DateTime");
          DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
          DateTime addMilliseconds = epoch.AddMilliseconds(timestampOne);
+         Console.WriteLine("Текущее UTC время: {0}", dateTime);
+         Console.WriteLine("Текущее UTC время в милисекундах: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTime);
+
 
          Console.WriteLine("Конвертация из Timestamp в DateTime: {0}", addMilliseconds);
          Console.WriteLine("Конвертация из Timestamp в DateTime с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", addMilliseconds);
