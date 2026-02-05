55f764aeb0259e0bfc8c7598546016d3202f4bdd
zenin5479 <zenin5479@gmail.com>
2026-02-05 22:27:57 +0300

    Update 37.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -162,7 +162,7 @@ namespace CurrentTime
          Console.WriteLine("Unix timestamp (ms): {0}", timestampfour);
 
          // Способ 5
-         Console.WriteLine("========================================================");
+         Console.WriteLine("================================================");
          Console.WriteLine("Способ 5. DateTimeOffset с явным преобразованием");
          Console.WriteLine("Текущее UTC время в миллисекундах: {0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.UtcNow);
          DateTimeOffset rightnow = DateTimeOffset.UtcNow;
