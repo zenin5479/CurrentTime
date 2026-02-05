f0c93d901adba51df912ed2373a84628f4d46746
zenin5479 <zenin5479@gmail.com>
2026-02-05 10:10:51 +0300

    Update 33.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -109,6 +109,10 @@ namespace CurrentTime
          Console.WriteLine("С учетом часового пояса: {0}", localZone);
          Console.WriteLine("С учетом часового пояса с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", localZone);
 
+         Console.WriteLine("========================================================");
+         Console.WriteLine("Конвертация из Timestamp в DateTime через DateTimeOffset");
+
+
          Console.WriteLine("Определение точного локального времени в миллисекундах");
          // Способ 1: DateTimeOffset (рекомендуется)
          DateTimeOffset datenow = DateTimeOffset.Now;
