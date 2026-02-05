bbc8d0e361979ecb7414c3f3aa89b3541b731e6c
zenin5479 <zenin5479@gmail.com>
2026-02-05 15:33:28 +0300

    Update 36.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -123,7 +123,7 @@ namespace CurrentTime
 
          // Конвертируем в локальное время
          Console.WriteLine("=====================================================");
-         Console.WriteLine("Конвертация из Timestamp в DateTime через ToLocalTime");
+         Console.WriteLine("Конвертация из Timestamp через TimeZoneInfo с учетом часового пояса");
          DateTime localZone = TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, timeZone);
          Console.WriteLine("С учетом часового пояса: {0}", localZone);
          Console.WriteLine("С учетом часового пояса с милисекундами: {0:dd.MM.yyyy HH:mm:ss.fff}", localZone);
